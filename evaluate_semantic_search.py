244ec26f1c27bb1f97f08282cf3689034506ed36
Victoria Ritvo <ritvo.v@gmail.com>
2025-10-20 16:22:24 -0400

    update evaluation raw table

--- evaluate_semantic_search.py
+++ evaluate_semantic_search.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import toml
 import os
-from utils import cross_encoder, initialize_models
+from utils import semantic_search, initialize_models
 import logging
 
 logger = logging.getLogger(__name__)
@@ -30,14 +30,18 @@ def evaluate_semantic_search(
     # Collect all results first, then create DataFrame once
     results = pd.DataFrame()
 
+    query_num = 0
     for listed_query, correct_answer in search_queries.items():
         search_query = listed_query
         print(search_query)
-        rows = cross_encoder(search_query, initial_k, final_k)
+        rows = semantic_search(search_query, initial_k)
 
         rows_df = pd.DataFrame(rows)
 
         # Add metadata column
+        rows_df["query"] = search_query
+        rows_df["query_num"] = query_num
+        rows_df["rank"] = rows_df.groupby("query").cumcount() +1
         rows_df["cross_encoder_model"] = cross_encoder_model
         rows_df["bi_encoder_model"] = bi_encoder_model
         rows_df["chunk_type"] = "window"  # Always window now
@@ -47,6 +51,8 @@ def evaluate_semantic_search(
         rows_df["initial_k"] = initial_k
         rows_df["final_k"] = final_k
 
+        # group by evaluation ID & query 
+
         rows_df["correct_match"] = rows_df["text"].str.contains(
             correct_answer, case=False, regex=False
         )
@@ -54,6 +60,7 @@ def evaluate_semantic_search(
         results = pd.concat([results, rows_df], ignore_index=True)
         results["date"] = pd.Timestamp.now().strftime("%Y-%m-%d-%H-%M-%S")
         results["evaluation_id"] = pd.util.hash_pandas_object(results).sum()
+        query_num += 1
 
     # Check if CSV exists and is non-empty
     header = True
@@ -66,6 +73,8 @@ def evaluate_semantic_search(
     # Save to CSV
     eval_df.to_csv(output_path, mode="a", index=False, header=header)
 
+    # 
+    
     return eval_df
 
 
