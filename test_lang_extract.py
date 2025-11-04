44ffacc5e1944bc7adea3c07e191049440dcb0ea
Victoria Ritvo <ritvo.v@gmail.com>
2025-11-04 12:35:51 -0500

    add examples

--- test_lang_extract.py
+++ test_lang_extract.py
@@ -10,13 +10,12 @@ API_KEY = os.getenv("LANGEXTRACT_API_KEY")
 config = toml.load("config.toml")
 
 print(API_KEY)
-input_text = """ A town square in Galway, Ireland, 1753. The camera looks straight down from above onto the cobblestones. A lone rider on his horse passes underneath, and the camera follows them past a well as Angelus narrates. 
-"""
+input_text = """Cut to Hemery High School in Los Angeles, 1996. School is over for the day, and the students come streaming out. An old, rusted Chevy Impala with its windows spray-painted black pulls up on the far side of the street. The driver's window lowers, and Angel squints out into the daylight, careful to remain in shadow. He looks over at the building and sees Buffy come down the steps with three of her friends. """
 
 prompt = textwrap.dedent("""\
     Extract location.
-    Use exact text for extractions. Do not paraphrase or overlap entities.
-    Provide meaningful attributes for each entity to add context.""")
+    Use exact text for extractions. Do not paraphrase or overlap entities. If the location is not certain, return n/a.
+    For the attribute location_descr, provide a short and concise description of the setting. If the town/city is stated and is not sunnydale in 1997 or later, that should be stated""")
 
 # 2. Provide a high-quality example to guide the model
 
@@ -35,52 +34,6 @@ examples = [
     )
     for example in config["NER"]["location"]["examples"]
 ]
-# examples = [
-#     lx.data.ExampleData(
-#         text="""Cut to the Summers house. Cut inside. Joyce is writing out a few bills. She hears a knocking at the door, and looks up. She goes over to the door and answers it. She is surprised at who she sees standing there. """,
-#         extractions=[
-#             lx.data.Extraction(
-#                 extraction_class="location",
-#                 extraction_text="Inside Buffy Summer's house",
-#                 attributes={"location_type": "indoors"},
-#             ),
-#         ],
-#     ),
-#     lx.data.ExampleData(
-#         text="""~~~~~~~~~~ Part 1 ~~~~~~~~~~""",
-#         extractions=[
-#             lx.data.Extraction(
-#                 extraction_class="location",
-#                 extraction_text="n/a",
-#                 attributes={"location_type": "n/a"},
-#             ),
-#         ],
-#     ),
-#     lx.data.ExampleData(
-#         text="""~~~~~~~~~~ Part 1 ~~~~~~~~~~
-#     Sunnydale High School. Cut to the hall. Cordelia is demonstrating her fake laugh to another girl.
-#     CORDELIA
-#     (fake laughter) See? Dr. Debi says when a man is speaking you make serious eye contact, and you really, really listen, and you laugh at everything he says. (laughs again)
-#     """,
-#         extractions=[
-#             lx.data.Extraction(
-#                 extraction_class="location",
-#                 extraction_text="Sunnydale High School Hall",
-#                 attributes={"location_type": "indoors"},
-#             ),
-#         ],
-#     ),
-#     lx.data.ExampleData(
-#         text="""Outside Angelus' mansion. The camera pans along its dark facade. Cut to the street. Buffy walks toward the mansion at a determined pace with the sword wrapped in a cloth. Suddenly Xander comes running out of the bushes on the hillside and jumps into the street in front of her. She startles and takes a reflexive step back.""",
-#         extractions=[
-#             lx.data.Extraction(
-#                 extraction_class="location",
-#                 extraction_text="Street outside Angelus's mansion",
-#                 attributes={"location_type": "outdoors"},
-#             ),
-#         ],
-#     ),
-# ]
 
 
 print(os.getenv("LANGEXTRACT_API_KEY"))
@@ -107,3 +60,6 @@ with open("visualization.html", "w") as f:
         f.write(html_content.data)  # For Jupyter/Colab
     else:
         f.write(html_content)
+
+
+# A town square in Galway, Ireland, 1753. The camera looks straight down from above onto the cobblestones. A lone rider on his horse passes underneath, and the camera follows them past a well as Angelus narrates.
